dc4b40a35ad280a8a8a31feb508a3a4290d77310
rafaelasantana <60327926+rafaelasantana@users.noreply.github.com>
2023-12-01 17:41:10 +0100

    after rebase

--- obj/Debug/net8.0/mtcg.AssemblyInfo.cs
+++ obj/Debug/net8.0/mtcg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mtcg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64348a7b0e3617beaaa117ef2da2de77c1774aa5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("mtcg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mtcg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
