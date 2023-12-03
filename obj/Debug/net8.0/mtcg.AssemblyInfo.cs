a6017ad7ac866f7b03b297971bb57368e795bda7
rafaelasantana <60327926+rafaelasantana@users.noreply.github.com>
2023-12-03 12:25:58 +0100

    package creation and user login/signin working

--- obj/Debug/net8.0/mtcg.AssemblyInfo.cs
+++ obj/Debug/net8.0/mtcg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mtcg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b09c7d9a410cafeb932313f18c542282357fbc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+345d3a89b00e0256a4efb9b03f7f3b5fc6b2d014")]
 [assembly: System.Reflection.AssemblyProductAttribute("mtcg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mtcg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
